b9f4b904e9b5d80db67624efe4110e4b0c1f67fa
deadblue <root@dead.blue>
2019-05-21 12:27:09 +0800

    Add GIF/PNG support.

--- include/class/imageconvertor.php
+++ include/class/imageconvertor.php
@@ -5,9 +5,11 @@ class ImageConvertor {
     private const DefaultJpegQuality = 80;
 
     private const FormatJpeg = "jpeg";
+    private const FormatGif = "gif";
+    private const FormatPng = "png";
 
     /**
-     * Convert image to jpeg format.
+     * Convert image to JPEG format.
      *
      * @param string $image
      * @param int $quality
@@ -27,7 +29,7 @@ class ImageConvertor {
     }
 
     /**
-     * Convert image to jpeg format and save to file.
+     * Save image to file in JPEG format.
      *
      * @param string $filename
      * @param string $image
@@ -44,6 +46,68 @@ class ImageConvertor {
         $im->readImageBlob($image);
         $im->setImageFormat(self::FormatJpeg);
         $im->setImageCompressionQuality($quality);
+        return $im->writeImage($filename);
+    }
+
+    /**
+     * Convert image to GIF format.
+     *
+     * @param string $image
+     * @return string
+     * @throws ImagickException
+     */
+    public static function toGif(string $image): string {
+        $im = new Imagick();
+        $im->readImageBlob($image);
+        $im->setImageFormat(self::FormatGif);
+        $im->setImageCompression(Imagick::COMPRESSION_LZW);
+        return $im->getImageBlob();
+    }
+
+    /**
+     * Save image to file in GIF format.
+     *
+     * @param string $filename
+     * @param string $image
+     * @return bool
+     * @throws ImagickException
+     */
+    public static function toGifFile(string $filename, string $image): bool {
+        $im = new Imagick();
+        $im->readImageBlob($image);
+        $im->setImageFormat(self::FormatGif);
+        $im->setImageCompression(Imagick::COMPRESSION_LZW);
+        return $im->writeImage($filename);
+    }
+
+    /**
+     * Convert image to PNG format.
+     *
+     * @param string $image
+     * @return string
+     * @throws ImagickException
+     */
+    public static function toPng(string $image): string {
+        $im = new Imagick();
+        $im->readImageBlob($image);
+        $im->setImageFormat(self::FormatPng);
+        $im->setOption('png:compression-level', 9);
+        return $im->getImageBlob();
+    }
+
+    /**
+     * Save image to file in PNG format.
+     *
+     * @param string $filename
+     * @param string $image
+     * @return bool
+     * @throws ImagickException
+     */
+    public static function toPngFile(string $filename, string $image): bool {
+        $im = new Imagick();
+        $im->readImageBlob($image);
+        $im->setImageFormat(self::FormatPng);
+        $im->setOption('png:compression-level', 9);
         return $im->writeImage($filename);
     }
 
